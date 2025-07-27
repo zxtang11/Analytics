1f44403496370e32f5b3dadea5e55db81703aec0
Ricardo Tang <zxtang11@gmail.com>
2025-07-27 18:09:19 +0100

    Initial clean commit

---
+++ payment_process.py
@@ -0,0 +1,15 @@
+import  streamlit as st
+
+st.title("Hello, Streamlit!, this is a test_new test to pull")
+st.write("Welcome to your first Streamlit app! haha")
+
+
+st.title("My Web Page!")
+st.header("About")
+st.write("This is my first webpage.")
+
+st.markdown("""
+- Item 1
+- orange
+- banana
+""")
